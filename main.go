b6312be37b8dbb60a7cdfb76a0f17a5923ac99dd
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2018-07-06 07:55:21 +0200

    add debug option -d

--- main.go
+++ main.go
@@ -20,6 +20,8 @@
 // 	# list all patterns in the order of the replacements
 //	uni -l
 //
+//	# show each rune line by line with it's code point
+//
 // Uni can be used in ktye/editor by selecting text and middle-clicking on |uni in the tag bar.
 package main
 
@@ -32,16 +34,32 @@ import (
 	"strings"
 )
 
+var debug bool
+
 func main() {
-	if len(os.Args) == 2 && os.Args[1] == "-h" {
+	args := os.Args
+
+	if len(args) == 2 && args[1] == "-h" {
 		fmt.Println(gen)
-	} else if len(os.Args) == 2 && os.Args[1] == "-l" {
+		return
+	} else if len(args) == 2 && args[1] == "-l" {
 		for _, e := range table {
 			fmt.Printf("%s %s\n", e[0], e[1])
 		}
-	} else if len(os.Args) > 1 {
-		for _, line := range os.Args[1:] {
-			translate(line + "\n")
+		return
+	}
+
+	if len(args) > 1 && args[1] == "-d" {
+		debug = true
+		args = args[1:]
+	}
+
+	if len(args) > 1 {
+		for i, line := range args[1:] {
+			translate(line)
+			if i < len(args[1:])-1 {
+				fmt.Println()
+			}
 		}
 	} else {
 		// We want to keep "\r\n" line endings.
@@ -68,7 +86,13 @@ func translate(s string) {
 	for _, e := range table {
 		s = strings.Replace(s, e[1], e[0], -1)
 	}
-	if _, err := io.WriteString(os.Stdout, s); err != nil {
-		log.Fatal(err)
+	if debug {
+		for _, r := range s {
+			fmt.Printf("%q %+q 0x%x 0%o\n", r, r, r, r)
+		}
+	} else {
+		if _, err := io.WriteString(os.Stdout, s); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
